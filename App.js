6965ec6f38a56bd13ba597953edab15ccd84e971
周浪 <lang.zhou@fulintechfin.com>
2019-03-30 21:44:05 +0800

    图标修改

--- App.js
+++ App.js
@@ -20,9 +20,9 @@ import {
   ScrollView
 } from 'react-native';
 import {
-  ThemeProvider, Button, Header
+  ThemeProvider, Button, Header,Input
 } from 'react-native-elements';
-import Icon from 'react-native-vector-icons/Octicons';
+import Icon from 'react-native-vector-icons/AntDesign';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -67,11 +67,20 @@ export default class App extends Component {
   };
 
   render() {
+    const InputComponent = () => (
+      <Input 
+        placeholder='马云高考数学1分'
+        placeholderTextColor='#fff'
+        leftIcon={<Icon name='search1' color='#fff' size={15}/>}
+        inputStyle={styles.searchInput}
+        inputContainerStyle={styles.searchInputContainer}/>
+    )
     return (
       <ThemeProvider theme={theme}>
         <Header
          leftComponent={{ text: '網易', style: { color:'#fff', fontSize: 18 } }}
-         rightComponent={<Icon name='beaker' color='#fff' size={20}/>}
+         centerComponent={InputComponent}
+         rightComponent={<Icon name='videocamera' color='#fff' size={20}/>}
         />
       </ThemeProvider>
     );
@@ -98,5 +107,18 @@ const styles = StyleSheet.create({
   buttonText: {
     padding: 20,
     color: 'red'
+  },
+  searchInputContainer: {
+    paddingLeft: 3,
+    paddingRight: 3,
+    borderBottomWidth: 0, 
+    borderRadius: 24,
+    height: 24,
+    backgroundColor: 'rgba(255, 255, 255, .5)'
+  },
+  searchInput: {
+    fontSize: 14, 
+    paddingLeft: 3, 
+    color: '#fff',
   }
 });
